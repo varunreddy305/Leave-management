b94741ed917d42bfff1dae83bf894d5aa81a5f17
Varun's Macbook <Varun's Macbook@DESKTOP-90Q8Q7M>
2020-06-05 11:44:10 -0400

    LeaveTypeController

--- Leave-management/Models/LeaveTypeViewModel.cs
+++ Leave-management/Models/LeaveTypeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Leave_management.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Display(Name="Date Created")]
         public DateTime DateCreated { get; set; }
     }
 
