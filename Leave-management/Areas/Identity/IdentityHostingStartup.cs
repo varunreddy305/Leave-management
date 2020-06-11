62db7d1d4d6e0748a0b01f9718b9722f53d288a6
Varun's Macbook <Varun's Macbook@DESKTOP-90Q8Q7M>
2020-06-11 15:20:02 -0400

    User Authentication

---
+++ Leave-management/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Leave_management.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Leave_management.Areas.Identity.IdentityHostingStartup))]
+namespace Leave_management.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
